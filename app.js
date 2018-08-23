8666e5c27502b01f259cc80dabdf4835db05134b
Samuel BR <samuelb1311@gmail.com>
2018-08-23 08:07:10 -0500

    load reddit posts from public json page, use lodash to unescape content of posts

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 new Vue({ 
     el: "#app",
+    data: {
+        posts: []
+    },
+    created() {
+        $.get("https://www.reddit.com/r/all/top.json?limit=50", (res) => {
+            this.posts = res.data.children;
+        });
+    },
     methods: {
-        goToPost() {
-            console.log("CLICKED POST")
+        goToPost(index) {
+            console.log("CLICKED POST", index)
         },
         dismissPost() {
             console.log("DISMISS POST")
+        },
+        hasThumbnail(itemData) {
+            return itemData.thumbnail.includes("http://") || itemData.thumbnail.includes("https://");
+        },
+        hoursPassed(created) {
+            let diff = (new Date().getTime() / 1000) - new Date(created).getTime();
+            diff /= (60 * 60);
+            return Math.abs(Math.floor(diff));
         }
     }
 })
