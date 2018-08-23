d7b54d0e923cdddd5e77640c723aa39f8200f56c
Samuel BR <samuelb1311@gmail.com>
2018-08-23 06:51:06 -0500

    created template and defined structure and layout design for posts in the list

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-new Vue({ el: '#app' })
+new Vue({ 
+    el: "#app",
+    methods: {
+        goToPost() {
+            console.log("CLICKED POST")
+        },
+        dismissPost() {
+            console.log("DISMISS POST")
+        }
+    }
+})
